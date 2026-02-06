1af270db3b3ab1563b62a9a5a20c84ae3fee1b3c
gamer <gamer91playz72@gmail.com>
2026-02-06 04:56:19 -0800

    Add eslint-config-prettier to SDK eslint config

--- sdk/typescript/eslint.config.js
+++ sdk/typescript/eslint.config.js
@@ -2,20 +2,26 @@ import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import nodeImport from "eslint-plugin-node-import";
+import prettier from "eslint-config-prettier";
 
-export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, {
-  plugins: {
-    "node-import": nodeImport,
-  },
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    plugins: {
+      "node-import": nodeImport,
+    },
 
-  rules: {
-    "node-import/prefer-node-protocol": 2,
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
+    rules: {
+      "node-import/prefer-node-protocol": 2,
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
   },
-});
+  prettier,
+);
